ca730a3871bbb12431a0aa95cfa06d262e78e0b1
Sherry <119943082+HonHonX@users.noreply.github.com>
2022-12-13 15:48:26 +0100

    Update fakultaet.c

--- fakultaet.c
+++ fakultaet.c
@@ -3,25 +3,27 @@
 int main () {
 
 	/*
-	*
+	* Liest eine Zahl n ein und gibt n! aus
 	*/
 
-	printf("\n *************************************** ");
-	printf("\n *   ^,,,,^   *                        * ");
-	printf("\n *  ( o x o)  *                        * ");
-	printf("\n *  ( 7   7)  *                        * ");
-	printf("\n *   ´´´´´´   *                ©S.Buch * ");
-	printf("\n *************************************** \n\n");
+	printf("\n ********************************* ");
+	printf("\n *   ^,,,,^   *                  * ");
+	printf("\n *  ( o x o)  * Fakultät-Rechner * ");
+	printf("\n *  ( 7   7)  *                  * ");
+	printf("\n *   ´´´´´´   *          ©S.Buch * ");
+	printf("\n ********************************* \n\n");
 	
-	//
+	//Einlesen des Werts für n
 	int ergebnis=1 ,n;
 	printf("\nBitte geben Sie eine Zahl ein: ");
 	scanf("%d",&n);
 
-	//
+	//Berechnung
 	for (int i=1; i<=n; i++) {
 		ergebnis*=i;
 	}
+		
+	//Ausgabe
 	printf("Das Produkt von 1 bis %d ist %d\n", n, ergebnis);
 
 	//Ende
